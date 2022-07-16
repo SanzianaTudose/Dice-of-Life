e1a440dadc4737bcff4589da506411df710bbbbb
SanzianaTudose <tudosesanziana@gmail.com>
2022-07-16 20:18:21 +0300

    Count Guess VI: determine final symbol

--- Assets/Scripts/Game/Scene VI/Row.cs
+++ Assets/Scripts/Game/Scene VI/Row.cs
@@ -6,14 +6,19 @@ using UnityEngine;
 // Controls movement
 public class Row : MonoBehaviour {
     private const float EPS = 0.4f;
+    private const float MAX_DIST = 100f;
 
     [SerializeField] private float rotInterval;
     [SerializeField] private float posUpperY, posLowerY;
     [SerializeField] private float rotSpeed;
     [SerializeField] private int rotDir;
+    [SerializeField] private float[] symbolPosY; 
+    [SerializeField] private string[] symbolString; 
     
     public bool rowStopped;
-    public string curSlotItem;
+    public string finalSymbol;
+
+    private int rotCount;
 
     private void Start() {
         rowStopped = true;
@@ -22,13 +27,16 @@ public class Row : MonoBehaviour {
     }
 
     private void StartRotating() {
-        curSlotItem = "";
+        finalSymbol = "";
+
         StartCoroutine(CoRotate());
     }
 
     private void StopRotating() {
         rowStopped = true;
-        // TODO: Determine stop position so it's centered
+
+        FindClosestSymbol();
+        MoveToClosestSymbol();
     }
 
     private IEnumerator CoRotate() {
@@ -36,15 +44,44 @@ public class Row : MonoBehaviour {
 
         // One loop gives one rotation
         while(!rowStopped) {
-            // When row reaches upper end, immediately shift it to lower end
-            // => Creates rotating illusion if both sides have the same symbol
-            if (Mathf.Abs(transform.position.y - posUpperY) <= EPS) 
-                transform.position = new Vector2(transform.position.x, posLowerY);
-
-            // Translate Row downwards by a set amount 
-            transform.position = new Vector2(transform.position.x, transform.position.y - rotDir * rotSpeed);  
+            rotCount++;
+            
+            RotateOnce();
 
             yield return new WaitForSeconds(rotInterval);
         }
     }
+
+    private void RotateOnce() {
+        // When row reaches upper end, immediately shift it to lower end
+        // => Creates rotating illusion if both sides have the same symbol
+        if (Mathf.Abs(transform.position.y - posUpperY) <= EPS)
+            transform.position = new Vector2(transform.position.x, posLowerY);
+
+        // Translate Row downwards by a set amount 
+        transform.position = new Vector2(transform.position.x, transform.position.y - rotDir * rotSpeed);
+    }
+
+    #region Stop Rotating Methods
+    private void FindClosestSymbol() {
+
+        // Find symbol that is closest to the stopped position
+        float minDist = MAX_DIST;
+        int closestInd = -1;
+        for (int i = 0; i < symbolPosY.Length; i++) {
+            float dist = Mathf.Abs(transform.position.y - symbolPosY[i]);
+
+            if (dist < minDist) {
+                minDist = dist;
+                closestInd = i;
+            }
+        }
+
+        finalSymbol = symbolString[closestInd];
+    }
+
+    private void MoveToClosestSymbol() {
+
+    }
+    #endregion
 }
